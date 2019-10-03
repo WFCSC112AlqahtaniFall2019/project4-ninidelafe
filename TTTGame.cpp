c3c1a1c8e4bd252cad48fa6246c1f866ebbb8b67
ninidelafe <delaam18@wfu.edu>
2019-10-03 16:13:54 -0400

    Initial Commit

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -1,5 +1,5 @@
 /* Class: TicTacToe
- * Author: [FINISH ME]
+ * Author: Alexa (Nini) de la Fe
  * Description: A program that implements the game Tic-Tac-Toe
  */
 
@@ -8,6 +8,28 @@
 using namespace std;
 
 int main() {
+
+    cout<<"Welcome to the Tic Tack Toe Game!"<<endl;
+    cout<<"Would you like to play a 1 or 2 person game?"<<endl;
+    int numPlayers;
+    cin>>numPlayers;
+
+    TicTacToe game;
+
+    game.initializeBoard();
+    bool winner=false;
+
+    if (numPlayers==1){
+
+
+    }
+    else if (numPlayers==2){
+        while(!game.checkForWin()) {
+            
+
+
+            }
+        }
 
     // FINISH ME
 
